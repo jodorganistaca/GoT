1279b5bf3b49f3e5bf118bc6757cbc732e06f145
Jose Daniel Organista <jodorganistaca@unal.edu.co>
2019-07-20 16:41:45 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var juego = new Phaser.Game(944,793, Phaser.CANVAS, "got");
 var fondo,fondoPix,cerrar,bolita;
 
 //gifs places
-var winterfell,castleBlack,greyjoy,casaArryn,lannisport,highGarden,dragonstone,casaTully; 
+var winterfell,castleBlack,greyjoy,casaArryn,lannisport,casaTully,highGarden,dragonstone,casaMartell,baratheon; 
 
 //Para los titulos
 var style1 = { font: "100px Times New Roman", fill: "#6797F1"};
@@ -15,6 +15,17 @@ var style5 = { font: "100px Times New Roman", fill: "#26872C"};
 //Para el contenido
 var style2 = { font: "18px Lucida Console", fill: "#ffffff", align: "justify"};
 
+
+//botones
+var castleBlackButton;
+var winterfellButton;
+var greyjoyButton;
+var casaArrynButton;
+var lannisportButton; 
+var casaTullyButton; 
+var highGardenButton; 
+var baratheonButton; 
+var casaMartellButton;
 
 //titulos 
 var t1, t2, t3, t4, t5, t6, t7, t8, t9;
@@ -27,7 +38,6 @@ var jugando = {
 	preload: function() {        // primero carga todas las imágenes
 		
 		juego.load.image("imgfondo", "imagenes/mapaGoT.jpg");
-		juego.load.image("imgPix", "imagenes/ninePix.jpeg");
 		//gifs señales
 		//juego.load.spritesheet('f1','imagenes/arrowGif.png',200,200,8);
 		//gifs places
@@ -39,20 +49,20 @@ var jugando = {
 		juego.load.spritesheet('highGarden','imagenes/highGarden.png',248,140,9);
 		juego.load.spritesheet('dragonstone','imagenes/dragonstone.png',480,268,50);
 		juego.load.spritesheet('casaTully','imagenes/casaTully.png',480,268,34);
-		/*juego.load.spritesheet('helheim','imagenes/helheim.png',540,304,30);
-		juego.load.spritesheet('niflheim','imagenes/niflheim.png',500,281,6);
+		juego.load.spritesheet('baratheon','imagenes/baratheon.png',250,170,34);
+		juego.load.spritesheet('casaMartell','imagenes/casaMartell.png',420,237,9);
 		//Imagenes botones
-		juego.load.image("button1","imagenes/buttonAsg.jpg");
-		juego.load.image("button2","imagenes/buttonAlf.jpg");
-		juego.load.image("button3","imagenes/buttonMid.jpg");
-		juego.load.image("button4","imagenes/buttonMus.jpg");
-		juego.load.image("button5","imagenes/buttonVan.jpg");
-		juego.load.image("button6","imagenes/buttonNiv.jpg");
-		juego.load.image("button7","imagenes/buttonJot.jpg");
-		juego.load.image("button8","imagenes/buttonHel.jpg");
-		juego.load.image("button9","imagenes/buttonNif.jpg");
+		juego.load.image("button1","imagenes/castleBlackButton.jpg");
+		juego.load.image("button2","imagenes/winterfellButton.jpg");
+		juego.load.image("button3","imagenes/greyjoyButton.jpg");
+		juego.load.image("button4","imagenes/casaArrynButton.jpg");
+		juego.load.image("button5","imagenes/lannisportButton.jpg");
+		juego.load.image("button6","imagenes/casaTullyButton.jpg");
+		juego.load.image("button7","imagenes/highGardenButton.jpg");
+		juego.load.image("button8","imagenes/baratheonButton.jpg");
+		juego.load.image("button9","imagenes/casaMartellButton.jpg");
 		//Imagen para cerrar la descripcións
-		juego.load.image("botoncerrar", "imagenes/botoncerrar.png");*/
+		//juego.load.image("botoncerrar", "imagenes/botoncerrar.png");
 		
 
 	},
@@ -62,10 +72,8 @@ var jugando = {
 		juego.scale.setMinMax(500, 336, 1000, 800); //se indica el tamaño minimo y maximo del canvas
 
 		//Se agregan las imagenes de ondo y oculta la pixelada
-		fondoPix = juego.add.sprite(0, 0, "imgPix");
-		fondoPix.visible=false;
 		fondo = juego.add.sprite(0, 0, "imgfondo");
-		
+		//fondo.visible=false;
 		//places Igualmente se crean las animaciones de los lugares y se ocultan
 		winterfell = juego.add.sprite(590,329,'winterfell',41);
 		winterfell.scale.setTo(0.5,0.7);
@@ -112,18 +120,28 @@ var jugando = {
 		var casaTullyA= casaTully.animations.add('casaTully');
 		casaTullyA.play(8, true);
 		casaTully.visible = false;
-
-		helheim = juego.add.sprite(250,330,'helheim',25);
-		helheim.scale.setTo(0.6,0.6);
-		var helheimA= helheim.animations.add('helheim');
-		helheimA.play(8, true);
-		helheim.visible = false;
+      
+		baratheon = juego.add.sprite(250,170,'baratheon',34);
+		var baratheonA= baratheon.animations.add('baratheon');
+		baratheonA.play(8, true);
+		baratheon.visible = false;
 		
-		niflheim = juego.add.sprite(250,330,'niflheim',6);
-		niflheim.scale.setTo(0.6,0.6);
-		var niflheimA= niflheim.animations.add('niflheim');
-		niflheimA.play(8, true);
-		niflheim.visible = false;
+		casaMartell = juego.add.sprite(420,237,'casaMartell',9);
+		casaMartell.scale.setTo(0.6,0.6);
+		var casaMartellA= casaMartell.animations.add('casaMartell');
+		casaMartellA.play(8, true);
+		casaMartell.visible = false;
+
+		//botones y agrega funciones para el momento de hacer clic mostar la info
+		castleBlackButton = juego.add.button(469,42, "button1",verCastleBlack,this);
+		winterfellButton = juego.add.button(431,163, "button2",verWinterfell,this);
+		greyjoyButton = juego.add.button(319,404, "button3",verGreyjoy,this);
+		casaArrynButton = juego.add.button(501,379, "button4",verCasaArryn,this);
+		lannisportButton = juego.add.button(347,500, "button5",verLannisport,this);
+		casaTullyButton = juego.add.button(408,	437, "button6",verCasaTully,this);
+		highGardenButton = juego.add.button(379,616, "button7",verHighGarden,this);
+		baratheonButton = juego.add.button(526,560, "button8",verBaratheon,this);
+		casaMartellButton = juego.add.button(514,718, "button9",verCasaMartell,this);
 		
 		
 		bolita = this.add.graphics();
@@ -139,6 +157,33 @@ var jugando = {
 		}
 	},
 
+}
+function verCastleBlack(){
+	castleBlack.visible = true;
+}
+function verWinterfell(){
+	winterfell.visible = true;
+}
+function verGreyjoy(){
+	greyjoy.visible = true;
+}
+function verCasaArryn(){
+	casaArryn.visible = true;
+}
+function verLannisport(){
+	lannisport.visible = true;
+}
+function verCasaTully(){
+	casaTully.visible = true;
+} 
+function verHighGarden(){
+	highGarden.visible = true;
+} 
+function verBaratheon(){
+	baratheon.visible = true;
+} 
+function verCasaMartell(){
+	casaMartell.visible = true;
 }
 
 juego.state.add("activo", jugando);   // define a "jugando" como un estado
